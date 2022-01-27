3d32d03656cd8e7a3e4225669d9b1b8dbc816a60
Ruben Schlaen <gschlaen@agro.uba.ar>
2022-01-27 17:49:34 -0300

    Creo instancia de flask

--- src/app.py
+++ src/app.py
@@ -1,1 +1,9 @@
-print('Hola Mundo')
+from flask import Flask
+from flask import render_template
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+
+if __name__ == '__main__':
+    app.run(debug=True)
